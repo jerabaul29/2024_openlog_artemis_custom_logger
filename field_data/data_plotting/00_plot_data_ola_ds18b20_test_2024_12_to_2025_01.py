7e8e5f215f47c8238725c7e2b4ddceaf82e397ba
JRblt <jean.rblt@gmail.com>
2025-01-07 15:52:16 +0100

    WIP

--- field_data/data_plotting/00_plot_data_ola_ds18b20_test_2024_12_to_2025_01.py
+++ field_data/data_plotting/00_plot_data_ola_ds18b20_test_2024_12_to_2025_01.py
@@ -11,13 +11,15 @@ ipython3
 
 from loguru import logger
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+
 # %%
 
-# %reset -f
-
-# %%
-
-# code here
+path_prototype_data = Path.cwd() / "../prototype_sensors/2025_01_07_data/"
+path_reference_data = Path.cwd() / "../official_reference_sensors/2025_01_07_data/"
 
 # %%
 
